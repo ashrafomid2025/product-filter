695692cee8f1152eede2479a53e8a8da5922a3db
ashrafomid2025 <omidmashraf@gmail.com>
2025-05-14 17:27:24 +0430

    changes in tailwindcss

---
+++ public/favBook.js
@@ -0,0 +1,58 @@
+const bookName = document.getElementById("bookName");
+const bookDescription = document.getElementById("bookDescription");
+const imgUrl = document.getElementById("imgUrl");
+const addBtn = document.getElementById("addBtn");
+
+const bookList = [];
+
+addBtn.addEventListener("click",addBook);
+function addBook(){
+    if(bookName.value !=="" && bookDescription.value !=="" && imgUrl.value!==""){
+    let book = {
+        name: bookName.value,
+        description: bookDescription.value,
+        imgUrl: imgUrl.value
+    }
+
+    bookList.push(book);
+    showList();
+    bookName.value = "";
+    bookDescription.value = "";
+    imgUrl.value = "";
+    const success = document.getElementById("success");
+    success.classList.remove("hidden");
+    setTimeout(()=>{
+        success.classList.add("hidden");
+    },3000);
+}else{
+    const warning = document.getElementById("warning");
+    warning.classList.remove("hidden");
+    setTimeout(()=>{
+        warning.classList.add("hidden");
+    },3000)
+}
+}
+
+
+// showing the book
+function showList(){
+    const bookDiv = document.getElementById("bookList");
+    bookDiv.innerHTML = "";
+    bookList.forEach((book)=>{
+        const div = document.createElement("div");
+        div.classList.add("bookDiv");
+        const image = document.createElement("img");
+        image.setAttribute("src",book.imgUrl)
+        image.classList.add("bookImg")
+        const h1 = document.createElement("h1");
+        h1.textContent = book.name;
+        const p = document.createElement("p");
+        p.textContent = book.description;
+
+
+        div.append(image);
+        div.append(h1);
+        div.append(p);
+        bookDiv.append(div);
+    })
+}
